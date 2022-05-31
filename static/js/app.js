29f841a1017fb59b9800e94ed0a4e2033ec2e005
markm3690 <mrmileyy@gmail.com>
2022-05-30 19:44:32 -0500

    add CSS styling and link JS

--- static/js/app.js
+++ static/js/app.js
@@ -7,18 +7,18 @@ var tbody = d3.select("tbody");
 // Insert data to table using function - keeping code self contained
 function buildTable(data) {
     //clear existing data
-    tbody.html('');
+    tbody.html("");
     // loop through each row object
     data.forEach((dataRow) => {
         // append table body with table row tag
-        let row = tbody.append('tr');
+        let row = tbody.append("tr");
         // loop through each row value object
         Object.values(dataRow).forEach((val) => {
             // append a cell to each value with table data tag
-            let cell = row.append('td');
+            let cell = row.append("td");
             // add the values from object location
             cell.text(val);
-        })
+        });
     });
 };
 
@@ -26,7 +26,7 @@ function buildTable(data) {
 // add function to hold our date data, filtered and unfiltered
 function handleClick() {
     // grab datetime value from filter
-    let date = d3.select('#datetime').property('value');
+    let date = d3.select("#datetime").property("value");
     // set variable equal to original table data as default
     let filteredData = tableData;
 
@@ -42,7 +42,7 @@ function handleClick() {
 };
 
 // Listen for click with D3 and then execute handleClick function
-d3.selectAll('#filter-btn').on('click', handleClick);
+d3.selectAll("#filter-btn").on("click", handleClick);
 
 // make sure table loads as soon as page does
 // call buildTable using original data
